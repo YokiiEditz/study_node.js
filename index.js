27d0a15874f5479dbfbb00cee46652547246497c
Yoki <thekingmaker0012@gmail.com>
2024-09-05 14:07:37 +0530

    commit

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const url = "mongodb://localhost/mern-crud-app";
-const PORT = 9601;
+
 const app = express();
 
-mongoose.connect(url);
-const con = mongoose.connection;
+const PORT = 9601;
 
-con.on("open", function () {
-  console.log("connected!");
+// const routers = require("./routers/all");
+
+// app.use("/all", routers);
+
+app.get("/", (req, res) => {
+  res.send({ message: "hello world!!!" });
 });
 
-const routers = require("./routers/all");
-
-app.use("/all", routers);
+app.get("/about", (req, res) => {
+  res.send("about page");
+});
 
 app.listen(PORT, () => console.log(`Server running on ${PORT} `));
